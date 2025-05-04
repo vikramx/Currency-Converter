cc6294a7058c7c1cd4e9fb636ca1c058446ea0bb
vikramx <30879085+vikramx@users.noreply.github.com>
2025-05-04 14:22:12 +0530

    Create Currency-Converter.py

---
+++ Currency-Converter.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import json
+import requests
+
+st.title("Currency Converter 💵💰")
+st.subheader("An app to convert different currencies")
+c_codes=[
+    "AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG", "AZN",
+    "BAM", "BBD", "BDT", "BGN", "BHD", "BIF", "BMD", "BND", "BOB", "BRL",
+    "BSD", "BTN", "BWP", "BYN", "BZD", "CAD", "CDF", "CHF", "CLP", "CNY",
+    "COP", "CRC", "CUP", "CVE", "CZK", "DJF", "DKK", "DOP", "DZD", "EGP",
+    "ERN", "ETB", "EUR", "FJD", "FKP", "FOK", "GBP", "GEL", "GGP", "GHS",
+    "GIP", "GMD", "GNF", "GTQ", "GYD", "HKD", "HNL", "HRK", "HTG", "HUF",
+    "IDR", "ILS", "IMP", "INR", "IQD", "IRR", "ISK",
+"JEP", "JMD", "JOD",
+    "JPY", "KES", "KGS", "KHR", "KID", "KMF", "KRW", "KWD", "KYD", "KZT",
+    "LAK", "LBP", "LKR", "LRD", "LSL", "LYD", "MAD", "MDL", "MGA", "MKD",
+    "MMK", "MNT", "MOP", "MRU", "MUR", "MVR", "MWK", "MXN", "MYR", "MZN",
+    "NAD", "NGN", "NIO", "NOK", "NPR", "NZD", "OMR", "PAB", "PEN", "PGK",
+    "PHP", "PKR", "PLN", "PYG", "QAR", "RON", "RSD", "RUB", "RWF", "SAR",
+    "SBD", "SCR", "SDG", "SEK", "SGD", "SHP", "SLE", "SOS", "SRD", "SSP",
+    "STN", "SYP", "SZL", "THB",
+"TJS", "TMT", "TND", "TOP", "TRY", "TTD",
+    "TVD", "TWD", "TZS", "UAH", "UGX", "USD", "UYU", "UZS", "VES", "VND",
+    "VUV", "WST", "XAF", "XCD", "XDR", "XOF", "XPF", "YER", "ZAR", "ZMW", "ZWL"
+]
+
+currency1=st.selectbox("Enter first currency here:",c_codes)
+currency2=st.selectbox("Enter currency to be converted into here:",c_codes)
+amt=st.number_input("Enter amount of first currency to be converted:",min_value=1)
+
+if st.button("Convert"):
+    api_key = "30bfe46b0e9540bdb978261f"
+    base_url = f"https://v6.exchangerate-api.com/v6/30bfe46b0e9540bdb978261f/pair/{currency1}/{currency2}/{amt}"
+    response = requests.get(base_url)
+    if response.status_code==200:
+        data = response.json()
+        st.success(f"The converted amount is: {data['conversion_result']} {currency2}")
+    else:
+        st.error("Please enter valid values 😡")
